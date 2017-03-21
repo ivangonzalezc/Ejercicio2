9ddc3615e6bce67b318335e71d5723ab5d901944
alumno <alumno>
2017-03-20 21:56:29 -0300

    Ejercicio 6 resuelto

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 //echo "Hola mundo";
-//Ejercicio 6
+
 
 //Array
 
@@ -8,7 +8,7 @@
 
 $arrayConstructor = array(1,2,3,4,5);
 
-var_dump($arrayConstructor);
+//var_dump($arrayConstructor);
 echo "<br>";
 
 //2- [0] =
@@ -28,8 +28,36 @@ $arrayPush = array();
 
 array_push($arrayPush,rand(1,9),rand(1,9),rand(1,9),rand(1,9),rand(1,9));
 
-var_dump($arrayPush);
+//var_dump($arrayPush);
 echo "<br>";
 
+
+//Ejercicio 6
+
+$arrayCorchete = array();
+$suma = 0;
+$promedio = 0;
+$cantidadNumeros = 5;
+
+for ($i=0; $i<$cantidadNumeros ; $i++) 
+{ 
+  $arrayCorchete[$i]= rand(1,1 0);
+  $suma = $suma + $arrayCorchete[$i];
+}
+
+$promedio = $suma/$cantidadNumeros;
+
+if($promedio > 6)
+{
+  echo "El promedio es mayor a 6";
+}
+else if ($promedio == 6)
+{
+    echo "El promedio es igual a 6";
+}
+else
+{
+    echo "El promedio es menor a 6";
+}
 
 ?>
