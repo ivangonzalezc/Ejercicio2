0683352c350c1edbcaf2a236d8b596fed1cfb829
alumno <alumno>
2017-03-20 21:30:59 -0300

    Array de corchetes

--- index.php
+++ index.php
@@ -10,4 +10,16 @@ $arrayConstructor = array(1,2,3,4,5);
 
 var_dump($arrayConstructor);
 
+//2- [0] =
+
+$arrayCorchete = array();
+for ($i=0; $i< 5 ; $i++) 
+{ 
+  $arrayCorchete[$i]= rand(1,100);
+}
+
+var_dump($arrayCorchete);
+
+
+
 ?>
